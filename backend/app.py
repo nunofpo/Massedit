ed0d1a928e45412e7f8ddfca826ac04adf0c3c5d
Nuno Oliveira <nuno@example.com>
2026-09-13 16:26:28 +0100

    fix: adiciona import em falta de Query no backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from fastapi import FastAPI, HTTPException, Body, UploadFile, File, Form
+from fastapi import FastAPI, HTTPException, Body, UploadFile, File, Form, Query
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
